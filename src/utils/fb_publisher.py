4d76689cb61df0b5b90a3a1d66d84649521b633e
Witold Nowogorski <wnowogor@redhat.com>
2026-01-31 23:25:44 +0100

    simple fb publisher

---
+++ src/utils/fb_publisher.py
@@ -0,0 +1,22 @@
+import requests
+
+
+class FacebookPublisher:
+    def __init__(self, page_id, access_token):
+        self.page_id = page_id
+        self.access_token = access_token
+
+    def publish(self, content):
+        payload = {
+            "message": content,
+            "access_token": self.access_token
+        }
+        url = f"https://graph.facebook.com/v24.0/{self.page_id}/feed"
+        response = requests.post(url, data=payload)
+
+        if response.status_code == 200:
+            post_id = response.json().get("id")
+            print(f"Post successfully published: {post_id}")
+        else:
+            print(f"Failed to upload post: {response.status_code}")
+            print(response.text)
